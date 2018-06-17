939b079f6df6afdc5f349cc53849a9c72bfddf7a
jason zheng <101104044@nccu.edu.tw>
2018-06-18 02:09:52 +0800

    剩下filter

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
-
-  constructor() { }
+  districts: any;
+  constructor() {}
 
   ngOnInit() {
+    this.districts = [
+      '桃源區',
+      '那瑪夏區',
+      '茂林區',
+      '六龜區',
+      '甲仙區',
+      '杉林區',
+      '美濃區',
+      '旗山區',
+      '內門區',
+      '田寮區',
+      '阿蓮區',
+      '路竹區',
+      '湖內區',
+      '茄萣區',
+      '永安區',
+      '岡山區',
+      '燕巢區',
+      '彌陀區',
+      '橋頭區',
+      '梓官區',
+      '楠梓區',
+      '大社區',
+      '大樹區',
+      '仁武區',
+      '左營區',
+      '旗山區',
+      '鳥松區',
+      '三民區',
+      '新興區',
+      '前金區',
+      '鹽埕區',
+      '苓雅區',
+      '鳳山區',
+      '前鎮區',
+      '旗津區',
+      '大寮區',
+      '小港區',
+      '林園區'
+    ];
   }
-
+  applyFilter(dist) {
+    console.log(dist);
+  }
 }
